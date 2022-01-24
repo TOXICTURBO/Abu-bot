bffce5aee8986b6743c4e6a331eae4b7a970c360
Ajx-Abu <98000024+Ajx-Abu@users.noreply.github.com>
2022-01-24 07:25:53 +0530

    Update xxxtentacion.js

--- plugins/xxxtentacion.js
+++ plugins/xxxtentacion.js
@@ -55,7 +55,7 @@ var i = Math.floor(15*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'ᴍᴀᴅᴇ ʙʏ sᴘɪʀɪᴛ sᴇʀ💝'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'ᴍᴀᴅᴇ ʙʏ ᴀʙᴜ sᴇʀ💝'})
 
     }));
 
@@ -104,7 +104,7 @@ r_text[15] = "https://telegra.ph/file/00a4732edf86b425ecdea.jpg";
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'ᴍᴀᴅᴇ ʙʏ sᴘɪʀɪᴛ sᴇʀ💝'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'ᴍᴀᴅᴇ ʙʏ ᴀʙᴜ sᴇʀ💝'})
 
     }));
 
