9bb3ca2bd628cbb6e324caf88077856499b40025
Ajx-Abu <98000024+Ajx-Abu@users.noreply.github.com>
2022-01-24 06:41:31 +0530

    Create clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -23,8 +23,8 @@ async function checkImAdmin(message, user = message.client.user.jid) {
 
 Asena.addCommand({pattern: 'clear', fromMe: true, desc: END, dontAddCommandList: true}, (async (message, match) => {
 
-    await message.sendMessage('```cleaning chat...```');
+    await message.sendMessage('```Abu Ser cleaning chat...```');
     await message.client.modifyChat (message.jid, ChatModification.delete);
-    await message.sendMessage('```🏳 Chat cleared 🏳```');
+    await message.sendMessage('```🏳 Abu Ser Chat cleared 🏳```');
 }));
  
