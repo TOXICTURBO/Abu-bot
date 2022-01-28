d2257016a7b03690aa1566be6a1950129a82f949
TOXICTURBO <97358745+TOXICTURBO@users.noreply.github.com>
2022-01-28 23:21:31 +0530

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -27,7 +27,7 @@ Asena.addCommand({pattern: 'info', fromMe: false, desc: 'Shows bot information a
     
 ╭─➤ 𝗜𝗡𝗙𝗢 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥 »
 │❖ *ᴀᴜᴛʜᴏʀ* : ` + Config.PLK + `
-│❖ *ᴋᴇʀɴᴇʟ* : ᴀʙᴜ
+│❖ *ᴋᴇʀɴᴇʟ* : Turbo
 │❖ *ɪɴꜱᴛᴀɢʀᴀᴍ* : ` + Config.SLINK + `
 │❖ *ᴡʜᴀᴛꜱᴀᴘᴘ* : ᴄᴏᴍɪɴɢ ꜱᴏᴏɴ...
 ╰────────────────❋ཻུ۪۪➹
